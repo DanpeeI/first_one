4aac12dc079d1191a786991788831e8f9c02f231
DanpeeI <127459337+DanpeeI@users.noreply.github.com>
2023-03-14 20:11:56 +0200

    1 file added
    clear
    
    clear

--- 1234.py
+++ 1234.py
@@ -1,2 +1,12 @@
-print('hello world')
-print('i have done it')
+a= ''
+a = a.upper()
+while a.upper() != 'HELLO':
+    a = input('Hello\n')
+    if a.upper() == 'HELLO':
+        a = input('WHAT`S UP?\n')
+        if a.upper() == 'GOOD':
+            print('Bye')
+            break
+        elif a.upper() != 'GOOD':
+            a = input("Why?")
+            break
